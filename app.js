04b01f2c7f62a0853cecfaf2f3ba16cb473978f3
Saikat Chattaraj <rick711c@gmail.com>
2025-07-19 14:34:45 +0530

    setup

---
+++ app.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById('root'));
+const elm1 = React.createElement("h1",{},'Hello world');
